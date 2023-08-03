2f1cdcb73e0d828a54ff84ab4fb268da30c0494b
guibedan <guilhermebedan123@gmail.com>
2023-08-03 09:10:54 -0300

    firstCommit

---
+++ src/components/home/Social.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Styles.scss'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/guilherme-bedan-1aa703249/" className="home__social-icon" target={"_blank"}>
+            <i className="uil uil-linkedin"></i>
+        </a>
+        
+        <a href="https://github.com/guibedan" className="home__social-icon" target={"_blank"}>
+            <i className="uil uil-github"></i>
+        </a>
+    </div>
+  );
+}
+
+export default Social;
+
